5c088eab741ddcacb37d86d294f0d012e76c16b7
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2023-09-15 09:17:39 -0500

    Add files via upload

---
+++ src/Paquete1/Class2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete1;
+import Paquete2.Class3;
+import Paquete2.Class4;
+import Paquete3.Class5;
+import Paquete3.Class6;
+
+
+public class Class2 {
+    public static void main(String[] args) {
+      Class1 clas = new Class1();
+      Class3 clas2 = new Class3();
+      Class4 clas3 = new Class4();
+      Class5 clas5 = new Class5();
+      Class6 clas4 = new Class6();
+      
+      clas.a = 12;
+      clas2.b = "";
+      clas3.b = "";
+      clas4.c = 1;
+      clas5.b = 2f;
+    }
+}
