c8336948e93fdc9eb357d30084accfd014a5f778
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2023-10-03 09:15:03 -0500

    Add files via upload

---
+++ S07_16/S07_16_FavioHuaman/Main_07_16_FavioHuaman.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S07_16_FavioHuaman;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main_07_16_FavioHuaman {
+    public static void main(String[] args) {
+        Persona1 per1 = new Persona1();
+        Persona2 per2 = new Persona2();
+        
+        per1.per1("Maria", 21);
+        per1.saludar();
+        per2.nombre();
+        per2.edad();
+        per2.per1("Jose", 20);
+        per2.saludar();
+    }
+}
---
+++ S07_16/S07_16_FavioHuaman/Persona1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S07_16_FavioHuaman;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Persona1 {
+    //Atributos
+    public String nombre;
+    public int edad;
+    
+    public void per1(String nombre,int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    public void saludar(){
+        System.out.println("Hola, soy " +this.nombre +" mi edad es " +this.edad);
+        
+    }
+    
+}
