197f42f914a82252fe27cd83c9b2ada255ae4e25
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2023-11-07 09:13:41 -0500

    Primer ejercicio

---
+++ 16_FavioHuaman/src/main/java/S15_16/Calculadora_16.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S15_16;
+
+/**
+ *
+ * @author Usuario23
+ */
+public class Calculadora_16 {
+    int a;
+    int b;
+    int c; 
+    int d;
+
+    public void suma(int a, int b){
+        int suma = a + b;
+    }
+    
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void resta(int b, int c){
+        int resta = b - c; 
+    }
+    
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void mult(){
+        int mult = a * d;
+    }
+    
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public void div(){
+        int div = d / c;
+    }
+    
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+    
+}
