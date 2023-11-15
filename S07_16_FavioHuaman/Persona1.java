2fb7a884ef40d9afc900b39be6fa9ff48dd69f32
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2023-11-15 18:27:21 -0500

    Update Persona1.java

--- S07_16_FavioHuaman/Persona1.java
+++ S07_16_FavioHuaman/Persona1.java
@@ -23,5 +23,10 @@ public class Persona1 {
         System.out.println("Hola, soy " +this.nombre +" mi edad es " +this.edad);
         
     }
+    public void cumplirAnios() {
+        // Incrementa la edad en un año
+        this.edad++;
+        System.out.println("¡Feliz cumpleaños! Ahora tengo " + this.edad + " años.");
+    }
     
 }
